df90a8252736ce97e7e2c15ba1054f14cbcdf9c8
YildirimKaral <y.karal@letvertise.de>
2016-02-03 09:46:33 +0100

    track IP Adresses behing proxies

--- routes/cookies2jsonp.js
+++ routes/cookies2jsonp.js
@@ -2,8 +2,10 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
+    var result = req.cookies;
+    result['cip'] = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
     res.setHeader('content-type', 'text/javascript');
-    res.jsonp(req.cookies);
+    res.jsonp(result);
 });
 
 module.exports = router;
